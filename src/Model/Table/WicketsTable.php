f098a0f3b94ef83f52ecd1c328d3ab22e1f65c03
David Yell <dyell@ukwebmedia.com>
2014-10-08 15:33:07 +0100

    Fixed the uses

--- src/Model/Table/WicketsTable.php
+++ src/Model/Table/WicketsTable.php
@@ -1,7 +1,6 @@
 <?php
 namespace App\Model\Table;
 
-use Cake\ORM\Query;
 use Cake\ORM\Table;
 use Cake\Validation\Validator;
 
@@ -42,8 +41,8 @@ class WicketsTable extends Table {
 /**
  * Default validation rules.
  *
- * @param \Cake\Validation\Validator $validator
- * @return \Cake\Validation\Validator
+ * @param Validator $validator
+ * @return Validator
  */
 	public function validationDefault(Validator $validator) {
 		$validator
