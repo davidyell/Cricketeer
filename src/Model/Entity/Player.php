979d2b8873c6033c835d19e6e957661ee085c9ee
David Yell <dyell@ukwebmedia.com>
2014-10-22 12:46:56 +0100

    Added missing docblock

--- src/Model/Entity/Player.php
+++ src/Model/Entity/Player.php
@@ -8,6 +8,11 @@ use Cake\ORM\Entity;
  */
 class Player extends Entity {
 
+/**
+ * Mutator method for getting the players full name
+ *
+ * @return string
+ */
 	protected function _getFullName() {
 		return $this->_properties['first_name'] . ' ' . $this->_properties['initials'] . ' ' . $this->_properties['last_name'];
 	}
