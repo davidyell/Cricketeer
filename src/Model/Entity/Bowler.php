ab31cd29d4adbeaa3c85eaeef577809ebb52f687
David Yell <dyell@ukwebmedia.com>
2014-10-29 12:42:14 +0000

    Added virtual field to bowler to count the wickets

--- src/Model/Entity/Bowler.php
+++ src/Model/Entity/Bowler.php
@@ -1,11 +1,20 @@
 <?php
 namespace App\Model\Entity;
 
+use BadMethodCallException;
 use Cake\ORM\Entity;
 
 /**
  * Bowler Entity.
  */
 class Bowler extends Entity {
+
+	protected function _getWicketCount() {
+		if (isset($this->_properties['innings']['wickets'])) {
+			return count($this->_properties['innings']['wickets']);
+		}
+
+		throw new BadMethodCallException('You must select Innings => Wickets tables.');
+	}
 
 }
