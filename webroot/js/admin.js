669ad13f98fc77ebabb777015db627b3d02f0685
David Yell <dyell@ukwebmedia.com>
2014-11-12 16:35:21 +0000

    Expanded the javascript to allow adding and removing of wickets also

--- webroot/js/admin.js
+++ webroot/js/admin.js
@@ -4,21 +4,25 @@
 $(function () {
 
 // Add another buttons on the score card
-    $('a.add[data-action=add-bowler]').click(function (f) {
+    $('a.add[data-action=add-bowler], a.add[data-action=add-wicket]').click(function (f) {
         f.preventDefault();
-        var bowlerDiv = $('fieldset.bowling div.bowler').last().clone();
-
-        $(bowlerDiv).find('input[type=hidden]').remove();
-        $(bowlerDiv).find('select option:selected').removeAttr('selected');
-
-        if ($(bowlerDiv).find('span.glyphicon.glyphicon-trash').length == 0) {
-            $(bowlerDiv).find('div.clearfix').before('<span class="glyphicon glyphicon-trash"></span>');
+        if ($(this).data('action') == 'add-bowler') {
+            var div = $('fieldset.bowling div.bowler').last().clone();
+        } else {
+            var div = $('fieldset.wickets div.wicket').last().clone();
         }
 
-        $(bowlerDiv).find('div.form-group').each(function (i, e) {
-            $(e).find('input[type=number]').removeAttr('value');
+        $(div).find('input[type=hidden]').remove();
+        $(div).find('select option:selected').removeAttr('selected');
 
-            var num = $(e).html().match(/innings-[0-9]+-bowlers-([0-9]+)/)[1],
+        if ($(div).find('span.glyphicon.glyphicon-trash').length == 0) {
+            $(div).find('div.clearfix').before('<span class="glyphicon glyphicon-trash"></span>');
+        }
+
+        $(div).find('div.form-group').each(function (i, e) {
+            $(e).find('input').removeAttr('value');
+
+            var num = $(e).html().match(/innings-[0-9]+-[a-z]+-([0-9]+)/)[1],
                 cnt = parseInt(num) + 1,
                 regex = new RegExp(num, 'gi'),
                 newName = $(e).children('input, select').attr('name').replace(regex, cnt),
@@ -28,13 +32,13 @@ $(function () {
             $(e).children('input, select').attr('id', newId);
         });
 
-        $(this).before(bowlerDiv);
+        $(this).before(div);
     });
 
 // Remove added bowlers
-    $('fieldset.bowling').on('click', 'span.glyphicon.glyphicon-trash', function (e) {
+    $('fieldset.bowling, fieldset.wickets').on('click', 'span.glyphicon.glyphicon-trash', function (e) {
         e.preventDefault();
-        $(this).parents('.bowler').remove();
+        $(this).parent('div').remove();
     });
 
 });
