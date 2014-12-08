74970a2259be0d04ca31d83c9065f0885dbb03cd
David Yell <dyell@ukwebmedia.com>
2014-12-08 14:07:19 +0000

    Set a rough default limit and also fixed a bug in the innings selector

--- webroot/js/admin.js
+++ webroot/js/admin.js
@@ -89,9 +89,9 @@ $(function () {
 
 // Scroll to fixed for running total
     for (i = 1; i <= 4; i++) {
-        var offset = 0;
+        var offset = 2607;
 
-        if ($('div.innings.one[data-inningnum=' + i + ']').length > 0) {
+        if ($('div.innings[data-inningnum=' + i + ']').length > 0) {
             offset = $('div.innings.one[data-inningnum=' + i + '] fieldset.batting div.batsman.eleven').offset().top + 50;
         }
 
