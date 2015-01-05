946d2dfad1f56bdc9d4ba791d7abe9c051871fe5
David Yell <dyell@ukwebmedia.com>
2015-01-05 08:32:13 +0000

    Fixed a change in the routes

--- config/routes.php
+++ config/routes.php
@@ -21,6 +21,8 @@
 use Cake\Core\Plugin;
 use Cake\Routing\Router;
 
+Router::defaultRouteClass('Route');
+
 Router::prefix('admin', function($routes) {
 	$routes->extensions(['json']);
 
@@ -42,18 +44,7 @@ Router::scope('/', function($routes) {
  */
 	$routes->connect('/pages/*', ['controller' => 'Pages', 'action' => 'display']);
 
-/**
- * Connect a route for the index action of any controller.
- * And a more general catch all route for any action.
- *
- * The `fallbacks` method is a shortcut for
- *    `$routes->connect('/:controller', ['action' => 'index'], ['routeClass' => 'InflectedRoute']);`
- *    `$routes->connect('/:controller/:action/*', [], ['routeClass' => 'InflectedRoute']);`
- *
- * You can remove these routes once you've connected the
- * routes you want in your application.
- */
-	$routes->fallbacks();
+	$routes->fallbacks('InflectedRoute');
 });
 
 /**
