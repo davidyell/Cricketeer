7079a43c8e5dfd4c81b170b98f1164d9cb7c67d3
David Yell <dyell@ukwebmedia.com>
2016-03-08 11:32:07 +0000

    Upgraded some more of the config files

--- config/bootstrap_cli.php
+++ config/bootstrap_cli.php
@@ -15,16 +15,16 @@
 use Cake\Core\Configure;
 use Cake\Core\Exception\MissingPluginException;
 use Cake\Core\Plugin;
-
 /**
  * Additional bootstrapping and configuration for CLI environments should
  * be put here.
  */
-
+// Set the fullBaseUrl to allow URLs to be generated in shell tasks.
+// This is useful when sending email from shells.
+//Configure::write('App.fullBaseUrl', php_uname('n'));
 // Set logs to different files so they don't have permission conflicts.
 Configure::write('Log.debug.file', 'cli-debug');
 Configure::write('Log.error.file', 'cli-error');
-
 try {
     Plugin::load('Bake');
 } catch (MissingPluginException $e) {
--- config/routes.php
+++ config/routes.php
@@ -21,7 +21,7 @@
 use Cake\Core\Plugin;
 use Cake\Routing\Router;
 
-Router::defaultRouteClass('InflectedRoute');
+Router::defaultRouteClass('DashedRoute');
 
 Router::prefix('admin', function($routes) {
 	$routes->extensions(['json']);
